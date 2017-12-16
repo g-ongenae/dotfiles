e1a417c97eb630daf8f71ff87bdefe45b4532154
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2017-12-16 18:00:53 +0100

    Install software used

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Check soft deps installed
+if [ "$(uname)" == "Darwin" ]; then
+    ! [ "$(jrnl --version)" == "" ] || brew install jrnl
+    ! [ "$(hub --version)" == "" ] || brew install hub
+    ! [ "$(git flow version)" == "" ] || brew install git-flow
+    ! [ "$(yarn --version)" == "" ] || brew install yarn --without-node
+fi
+
 # Get current dir (so run this script from anywhere)
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
