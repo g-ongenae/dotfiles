28b8a4e7bd3bd740b982893ff3f4157170d2df9f
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2020-01-30 11:12:12 +0100

    Don't stop because of brew, skip it

--- install.sh
+++ install.sh
@@ -39,12 +39,90 @@ function install_brew
   fi
 }
 
-if [ "$(uname)" != "Darwin" ] ; then
-  # TODO handle linux
-  bold "Unable to install programs: need MacOS"
+function install_all_tools
+{
+  # Piping stderr to /dev/zero (2>/dev/zero) to ignore error
 
-  exit
-fi
+  # Check soft deps installed
+  xcode-select --install 2>/dev/zero
+
+  if [ "$(brew --version 2>/dev/zero)" == "" ] ; then
+    bold "Installing Homebrew";
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  else
+    bold "Updating Homebrew";
+    brew update
+  fi
+
+  bold "Check or install useful softwares";
+  # Shell useful command
+  install_brew "tree"
+  install_brew "gpg"
+
+  # Lang
+  ! [ "$(go version 2>/dev/zero)" == "" ] || open https://golang.org/doc/install
+  ! [ "$(ghc --version 2>/dev/zero)" == "" ] || open https://www.haskell.org/downloads
+  install_brew "node"
+  install_brew "nvm"
+
+  if [ "$(rvm --version 2>/dev/zero)" == "" ]; then
+    gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    curl -sSL https://get.rvm.io | bash -s stable
+  fi
+
+  # Journal
+  install_brew "jrnl"
+
+  # Git
+  install_brew "hub"
+  install_brew "git-flow"
+  ! [ "$(overcommit --version 2>/dev/zero)" == "" ] || gem install overcommit
+
+  # Ops
+  install_brew "mongodb"
+  ! [ "$(docker version 2>/dev/zero)" == "" ] || open https://store.docker.com/editions/community/docker-ce-desktop-mac
+  install_brew "kubectl"
+  install_brew "kube-ps1"
+
+  # Linters / validators
+  ! [ "$(circleci version 2>/dev/zero)" == "" ] || curl https://raw.githubusercontent.com/CircleCI-Public/circleci-cli/master/install.sh --fail --show-error | bash
+  ! [ "$(travis version 2>/dev/zero)" == "" ] || gem install travis
+  install_brew "shellcheck"
+  install_brew "hadolint"
+  ! [ "$(mdl --version 2>/dev/zero)" == "" ] || gem install mdl
+}
+
+function install_all_cask
+{
+  # Cask installs
+  bold "Install Cask"
+  install_brew "cask"
+
+  install_cask "insomnia"
+  install_cask "postman"
+  install_cask "vectr"
+  install_cask "1Clipboard"
+  install_cask "Dashlane"
+  install_cask "hyper"
+  install_cask "Studio\ 3T" "studio-3t"
+  install_cask "Github\ Desktop" "github-desktop"
+  install_cask "Visual\ Studio\ Code" "visual-studio-code"
+  install_cask "Google\ Chrome" "google-chrome"
+  ! [ -f "/Applications/Station.app" ] || open https://dl.getstation.com/download/osx
+  ! [ -f "/Applications/Cliqz.app" ] || open https://cdn.cliqz.com/browser-f/download/web0001/CLIQZ.en-US.mac.dmg
+}
+
+function install_all_npm
+{
+  # TODO install vscode & hyper plugins
+
+  bold "Update NPM"
+  npm i -g npm
+
+  bold "Install or update globally NPM modules"
+  npm i -g pug-lint ember-template-lint eslint tslint prettier sass-lint \
+    http-server swagger-editor-live typescript fx
+}
 
 # Create Documents architecture
 ensure_dir "prog"
@@ -60,81 +138,15 @@ if ! [ -d "${HOME}/Documents/prog/dotfiles" ] ; then
   git clone https://github.com/g-ongenae/dotfiles.git dotfiles
 fi
 
-# Piping stderr to /dev/zero (2>/dev/zero) to ignore error
-
-# Check soft deps installed
-xcode-select --install 2>/dev/zero
-
-if [ "$(brew --version 2>/dev/zero)" == "" ] ; then
-  bold "Installing Homebrew";
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [ "$(uname)" != "Darwin" ] ; then
+  # TODO handle linux
+  bold "Unable to install programs: need MacOS"
 else
-  bold "Updating Homebrew";
-  brew update
+  bold "Starting to install programs"
+  install_all_tools
+  install_all_cask
+  install_all_npm
 fi
-
-bold "Check or install useful softwares";
-# Shell useful command
-install_brew "tree"
-install_brew "gpg"
-
-# Lang
-! [ "$(go version 2>/dev/zero)" == "" ] || open https://golang.org/doc/install
-! [ "$(ghc --version 2>/dev/zero)" == "" ] || open https://www.haskell.org/downloads
-install_brew "node"
-install_brew "nvm"
-
-if [ "$(rvm --version 2>/dev/zero)" == "" ]; then
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-  curl -sSL https://get.rvm.io | bash -s stable
-fi
-
-# Journal
-install_brew "jrnl"
-
-# Git
-install_brew "hub"
-install_brew "git-flow"
-! [ "$(overcommit --version 2>/dev/zero)" == "" ] || gem install overcommit
-
-# Ops
-install_brew "mongodb"
-! [ "$(docker version 2>/dev/zero)" == "" ] || open https://store.docker.com/editions/community/docker-ce-desktop-mac
-install_brew "kubectl"
-install_brew "kube-ps1"
-
-# Linters / validators
-! [ "$(circleci version 2>/dev/zero)" == "" ] || curl https://raw.githubusercontent.com/CircleCI-Public/circleci-cli/master/install.sh --fail --show-error | bash
-! [ "$(travis version 2>/dev/zero)" == "" ] || gem install travis
-install_brew "shellcheck"
-install_brew "hadolint"
-! [ "$(mdl --version 2>/dev/zero)" == "" ] || gem install mdl
-
-# Cask installs
-bold "Install Cask"
-install_brew "cask"
-
-install_cask "insomnia"
-install_cask "postman"
-install_cask "vectr"
-install_cask "1Clipboard"
-install_cask "Dashlane"
-install_cask "hyper"
-install_cask "Studio\ 3T" "studio-3t"
-install_cask "Github\ Desktop" "github-desktop"
-install_cask "Visual\ Studio\ Code" "visual-studio-code"
-install_cask "Google\ Chrome" "google-chrome"
-! [ -f "/Applications/Station.app" ] || open https://dl.getstation.com/download/osx
-! [ -f "/Applications/Cliqz.app" ] || open https://cdn.cliqz.com/browser-f/download/web0001/CLIQZ.en-US.mac.dmg
-
-# TODO install vscode & hyper plugins
-
-bold "Update NPM"
-npm i -g npm
-
-bold "Install or update globally NPM modules"
-npm i -g pug-lint ember-template-lint eslint tslint prettier sass-lint \
-  http-server swagger-editor-live typescript fx
 
 # Get current dir (so run this script from anywhere)
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
