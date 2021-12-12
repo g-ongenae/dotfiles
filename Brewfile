f8e28d2c7b47623a5c36640eaddba30ab7bfe74c
Guillaume Ongenae <guillaume.ongenae@algoan.com>
2021-12-12 15:53:06 +0100

    Add Brewfile

---
+++ Brewfile
@@ -0,0 +1,188 @@
+# Preflight
+
+tap 'homebrew/cask'
+tap 'homebrew/cask-drivers'
+tap 'homebrew/cask-fonts'
+tap 'homebrew/cask-versions'
+tap 'homebrew/core'
+tap 'homebrew/boneyard'
+tap 'homebrew/dev-tools'
+tap 'homebrew/bundle'
+tap 'homebrew/services'
+
+# Web browser
+
+cask 'finicky'
+cask 'firefox'
+cask 'brave-browser'
+tap 'browsh-org/homebrew-browsh'
+brew 'browsh'
+
+# Chat
+
+cask 'discord'
+cask 'slack'
+
+# Environment-related
+
+cask 'raycast'
+cask 'karabiner-elements'
+cask 'rescuetime'
+
+# Terminals
+
+cask 'tabby'
+
+# Tmux is a newer terminal multiplexer.
+brew 'pkg-config', link: true
+brew 'tmux'
+brew 'reattach-to-user-namespace'
+
+# Shells
+
+brew 'zsh'
+brew 'zsh-completions'
+brew 'zsh-lovers'
+brew 'starship'
+brew 'coreutils'
+brew 'bat'
+brew 'less'
+brew 'most'
+brew 'delta'
+brew 'wdiff'
+brew 'exa'
+brew 'tree'
+brew 'fd'
+brew 'pcre'
+brew 'pcre++'
+brew 'fzf'
+brew 'zoxide'
+brew 'watch'
+brew 'gnu-which'
+brew 'gpg'
+brew 'curl', link: true
+brew 'wget'
+brew 'ed'
+brew 'gnu-sed'
+brew 'gawk'
+brew 'grep'
+brew 'ripgrep'
+brew 'jq'
+brew 'fx'
+brew 'yq'
+brew 'yh'
+brew 'unzip'
+brew 'gzip'
+brew 'gpatch'
+brew 'make'
+brew 'automake'
+brew 'gdb'  # gdb requires further actions to make it work. See `brew info gdb`.
+brew 'openssl'
+brew 'readline'
+brew 'rlwrap'
+brew 'base64'
+brew 'translate-shell'
+brew 'jrnl'
+
+# Helpers
+
+brew 'denisidoro/tools/navi'
+brew 'tldr'
+brew 'howdoi'
+
+# Git
+
+brew 'git'
+brew 'git-lfs'
+brew 'git-ftp'
+brew 'git-delta'
+brew 'github/gh/gh'
+brew 'hub'
+
+cask 'gitup'
+cask 'github-desktop'
+
+# Editors
+
+brew 'vim'
+cask 'visual-studio-code'
+cask 'typora'
+
+# Ops
+
+brew 'docker'
+cask 'virtualbox'
+brew 'gcloud'
+brew 'heroku'
+cask 'osxfuse'
+brew 'telepresence'
+cask 'proxyman'
+cask 'insomnia'
+brew 'tcl-tk'
+cask 'kube-solo'
+brew 'kubectl'
+brew 'minikube'
+brew 'kubernetes-cli'
+brew 'kube-ps1'
+brew 'terraform'
+
+# Database
+
+brew 'mongodb'
+brew 'mongodb-community'
+brew 'mongodb-database-tools'
+cask 'mongodb-compass-isolated-edition'
+brew 'mongosh'
+
+# Fonts
+
+brew 'fontconfig'
+brew 'freetype'
+
+# Langs
+
+brew 'gcc'
+
+brew 'go'
+
+brew 'openjdk'
+
+brew 'ghc'
+brew 'cabal-install'
+
+brew 'node'
+brew 'nvm'
+brew 'dum'
+
+brew 'python3'
+brew 'pyenv'
+brew 'pyenv-virtualenv'
+
+brew 'rvm'
+brew 'ruby'
+brew 'ruby-install'
+
+brew 'rust'
+brew 'rustup-init'
+
+brew 'jupyterlab'
+
+# Linters
+
+brew 'pandoc', link: true
+brew 'shellcheck'
+brew 'hadolint'
+brew 'prettier'
+
+# Android
+cask 'android-studio'
+cask 'android-file-transfer'
+cask 'android-messages'
+cask 'android-ndk'
+cask 'android-platform-tools'
+cask 'android-sdk'
+
+# Selenium
+brew 'selenium-server-standalone'
+brew 'chromedriver'
+brew 'geckodriver'
--- install.sh
+++ install.sh
@@ -15,42 +15,9 @@ function ensure_dir
   fi
 }
 
-# function install_yh
-# {
-#   # Yaml display
-#   # https://github.com/andreazorzetto/yh
-# }
-
-# Install a cask software
-function install_cask
-{
-  local CASK_NAME="${1}"
-  local CASK_INSTALL="${2}"
-
-  if [ "${CASK_INSTALL}" == "" ] ; then
-    CASK_INSTALL="${CASK_NAME}"
-  fi
-
-  if ! [ -f "/Applications/${CASK_NAME}.app" ] ; then
-    brew cask install "${CASK_INSTALL}"
-  fi
-}
-
-function install_brew
-{
-  local NAME="${1}"
-
-  if ! [ "$(brew list | grep "${NAME}")" == "" ] ; then
-    brew install "${NAME}"
-  fi
-}
-
 function install_all_tools
 {
-  # Piping stderr to /dev/zero (2>/dev/zero) to ignore error
-
-  # Check soft deps installed
-  xcode-select --install 2>/dev/zero
+  xcode --select install 2>/dev/zero
 
   if [ "$(brew --version 2>/dev/zero)" == "" ] ; then
     bold "Installing Homebrew";
@@ -60,63 +27,14 @@ function install_all_tools
     brew update
   fi
 
-  bold "Check or install useful softwares";
-  # Shell useful command
-  install_brew "tree"
-  install_brew "gpg"
-  install_brew "zoxide"
-
-  # Lang
-  ! [ "$(go version 2>/dev/zero)" == "" ] || open https://golang.org/doc/install
-  ! [ "$(ghc --version 2>/dev/zero)" == "" ] || open https://www.haskell.org/downloads
-  install_brew "node"
-  install_brew "nvm"
+  brew bundle --file "${HOME}/Documents/prog/dotfiles/Brewfile"
 
   if [ "$(rvm --version 2>/dev/zero)" == "" ]; then
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
     curl -sSL https://get.rvm.io | bash -s stable
   fi
 
-  # Journal
-  install_brew "jrnl"
-
-  # Git
-  install_brew "github/gh/gh"
-  install_brew "hub"
-  install_brew "git-flow"
-  ! [ "$(overcommit --version 2>/dev/zero)" == "" ] || gem install overcommit
-
-  # Ops
-  install_brew "mongodb"
-  ! [ "$(docker version 2>/dev/zero)" == "" ] || open https://store.docker.com/editions/community/docker-ce-desktop-mac
-  install_brew "kubectl"
-  install_brew "kube-ps1"
-
-  # Linters / validators
-  ! [ "$(circleci version 2>/dev/zero)" == "" ] || curl https://raw.githubusercontent.com/CircleCI-Public/circleci-cli/master/install.sh --fail --show-error | bash
-  ! [ "$(travis version 2>/dev/zero)" == "" ] || gem install travis
-  install_brew "shellcheck"
-  install_brew "hadolint"
-  ! [ "$(mdl --version 2>/dev/zero)" == "" ] || gem install mdl
-}
-
-function install_all_cask
-{
-  # Cask installs
-  bold "Install Cask"
-  install_brew "cask"
-
-  install_cask "insomnia"
-  # install_cask "1Clipboard"
-  # install_cask "Dashlane"
-  install_cask "typora" # or open https://typora.io/download/Typora.dmg
-  # install_cask "Studio\ 3T" "studio-3t"
-  install_cask "Github\ Desktop" "github-desktop"
-  install_cask "Visual\ Studio\ Code" "visual-studio-code"
-  install_cask "Google\ Chrome" "google-chrome"
-  # ! [ -f "/Applications/Cliqz.app" ] || open https://cdn.cliqz.com/browser-f/download/web0001/CLIQZ.en-US.mac.dmg
-  # ! [ -f "/Applications/Perimeter81.app" ] || open https://www.perimeter81.com
-  # ! [ -f "/Applications/Scratches.app" ] || open https://github.com/heapwolf/scratches
+  ! [ -f "/Applications/Perimeter81.app" ] || open https://www.perimeter81.com
 }
 
 function install_all_npm
@@ -153,11 +71,8 @@ if [ "$(uname)" != "Darwin" ] ; then
   # TODO handle linux
   bold "Unable to install programs: need MacOS"
 else
-  xcode --select install
-
   bold "Starting to install programs"
   install_all_tools
-  install_all_cask
   install_all_npm
 fi
 
