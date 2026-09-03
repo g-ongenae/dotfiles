2f3c9eb38aea8970cf73b542bf15dd53ac112063
Guillaume Ongenae <guillaume.ongenae@algoan.com>
2026-09-03 16:12:31 +0200

    Add dev container alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -311,5 +311,37 @@ alias ip="echo -e \"Local IP: \$(local_ip); Distant IP: \$(distant_ip)\""
 alias update_repos="~/Documents/prog/dotfiles/scripts/update-repos.sh"
 alias update_deps="~/Documents/prog/dotfiles/scripts/update-deps.sh"
 
+# Devcontainer
+function dev()
+{
+  cd ~/Documents/work || exit 1
+
+  if [[ -z "$1" ]]; then
+    echo "Usage: dev <command>"
+    echo "Commands:"
+    echo "  build - Build the devcontainer"
+    echo "  up - Run the devcontainer"
+    echo "  shell - Open a shell in the devcontainer"
+    echo "  ai - Open a shell in the devcontainer and run claude"
+    echo "  clean - Clean the devcontainer"
+    return 1
+  fi
+
+  case "$1" in
+    build) devcontainer build --workspace-folder . ;;
+    up) devcontainer up --workspace-folder . ;;
+    shell) devcontainer exec --workspace-folder . zsh ;;
+    ai) devcontainer exec --workspace-folder . claude ;;
+    clean)
+      docker rm -f $(docker ps -a -q | grep 'features' | awk '{print $1}' | head -n 1)
+      docker rmi -f $(docker images -a | grep 'features' | awk '{print $2}' | head -n 1)
+      ;;
+    *)
+      echo "Invalid command. Use 'dev build', 'dev up', 'dev shell', 'dev ai', or 'dev clean'."
+      return 1
+      ;;
+  esac
+}
+
 # Add secret aliases
 source ~/Documents/prog/dotfiles/zsh/secret/alias.zsh
